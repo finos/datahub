7cdf47622fa714e2f3144417a10549b29605d88f
Paul Groves <62599442+grovesy@users.noreply.github.com>
2020-05-18 11:29:22 +0100

    updated documentation

--- datahub_core/generators/data_frame/normal_sampler.py
+++ datahub_core/generators/data_frame/normal_sampler.py
@@ -1,10 +1,57 @@
-#pylint:disable=unused-argument
+#pylint:disable=unused-argument,line-too-long
 import functools
 from ... import metrics as fr_metrics
 from ..attribute_generators import NormalGenerator
 
 @fr_metrics.timeit
 def normal_sampler(data, mu=0, sigma=None):
+    """
+    Picks elements from an array to a Normal distribution. Probability of selection
+    is in the order of the data array, e.g. data[0] has a higher probability than
+    data[1], data[2], data[3] and so on.
+
+    ## Arguments:
+    data, array
+
+    Array of elements to select from. Items at the start of the array have a higher
+    probability than items at the back of the array
+
+    mu, float:
+    The mean https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.random.normal.html
+
+    sigma, float:
+    The standard deviation https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.random.normal.html
+
+    ## Returns:
+
+    ## Example:
+    import numpy as np
+    import datahub_core.generators as gen
+    import datahub_core.data as data
+
+    def test_correct_number_of_rows_are_generated():
+
+        df = gen.generate_from_model(
+            props={
+                'country': gen.normal_sampler(
+                    mu=1,
+                    data=data.countries())
+            },
+            count=100000,
+            randomstate=np.random.RandomState(13031981)
+        ).to_dataframe()
+
+        df['country'] = df['country'].map(lambda x: x.alpha2_code)
+
+
+        ax = df['country'].value_counts().plot(kind='bar')
+        ax = df['country'].value_counts().plot(x='month', linestyle='-', marker='o', ax=ax)
+        ax.set_xlabel("Country")
+        ax.set_ylabel("Count")
+        plt.show()
+
+    """
+
     return functools.partial(__normal_sampler, data=data, mu=mu, sigma=sigma)
 
 @fr_metrics.timeit
